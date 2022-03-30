6b197c2771ad2967c979836f05138712edfcebbb
Elios630 <78980830+Elios630@users.noreply.github.com>
2022-03-30 20:57:20 -0300

    [UPDATE](Issue #227 #231): api integração do Back-end
    Criação de api para integração do Back-end da RF01

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
-const valor = require("../../api/services/scraping/equivalencias.json")
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000
+const cursos = require("./services/scraping/equivalencias.json")
 
-app.use((req, res, next) => {
-    res.header({"Access-Control-Allow-Origin": "*"});
-    next();
-}) 
+app.listen(
+    PORT,
+    () => console.log(`Em: http://localhost:${PORT}`)
+)
 
-app.get('/equivalencias', (req,res) =>{
-    res.send(valor)
-})
+//Procura as equivalencias da materia
+app.get('/equivalencias/:id', (req,res) =>{
 
+    //Identifica a materia
+    const id = req.params.id.toUpperCase()
+    console.log(id.toUpperCase())
 
-app.listen(PORT,() => {
-    console.log(`Escutando na porta ${PORT}`)
+    //Procura a materia dentro do equivalencias.json
+    for(var i = 0; i < cursos.length; i++){
+        if (id == cursos[i].name){
+            curso = cursos[i].nameSub
+            console.log(curso)
+            if (curso == ""){
+                res.send('Não tem equivalente.')
+            }else{
+                res.send(curso)
+            }
+            break
+        }
+    }  
+    if (i == cursos.length) res.send('Matéria não encontrada.')
 })
