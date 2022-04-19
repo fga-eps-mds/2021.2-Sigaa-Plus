cf45ba898f75c960c74a17048abf487786e606c2
GabriellyAssuncao <gabriellyassuncaorodrigues@gmail.com>
2022-04-19 10:54:33 -0300

    [ADD] (Issue #228) : salvando matérias cursadas no localStorage

--- api/services/scraping/materiasCursadas.js
+++ api/services/scraping/materiasCursadas.js
@@ -1,10 +1,13 @@
-console.log("Hello World!")
+let doc = document.querySelectorAll("#j_id_jsp_303365748_2 > table > tbody:nth-child(4) > tr > td:nth-child(1)")
+const matList = [...doc]
+const materias = matList.map(({ innerText }) => ({
+    materia: innerText
+}))
+console.log(materias)
 
-window.onload = function salvarMaterias() {
-    //capturando os dados das matérias dentro do formulário
-    let doc = document.querySelectorAll("#j_id_jsp_303365748_2 > table > tbody:nth-child(4) > tr > td:nth-child(1)")
-    const matList = [...doc]
-    const materias = matList.map(({ innerText }) => ({
-        materia: innerText
-    }))
-}
+localStorage.setItem("materias", JSON.stringify(materias))
+console.log('Matérias salvas com sucesso');
+
+//----Retornando matérias salvas no local storage----
+// const disciplinas = JSON.parse(localStorage.getItem('materias'));
+// console.log(disciplinas);
