3cb15aa90a1e820525e8ac538c726c3a1cee06fc
Hian Praxedes <78980856+HianPraxedes@users.noreply.github.com>
2022-04-19 19:12:25 -0300

    [ADD](Issue #167) : feature-RF02

---
+++ api/routes/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000
+const cursos = require("./services/scraping/equivalencias.json")
+var curso
+
+app.listen(
+    PORT,
+    () => console.log(`Em: http://localhost:${PORT}`)
+)
+
+//Procura as equivalencias da materia
+app.get('/equivalencias/:id', (req,res) =>{
+
+    //Identifica a materia
+    const id = req.params.id.toUpperCase()
+
+    //Procura a materia dentro do equivalencias.json
+    for(var i = 0; i < cursos.length; i++){
+        if (id == cursos[i].name){
+            curso = cursos[i].nameSub
+            if (curso == ""){
+                res.send('Não tem equivalente.')
+            }else{
+                res.send(curso)
+            }
+            break
+        }
+    }  
+    if (i == cursos.length) res.send('Matéria não encontrada.')
+})
