c3c30fa00a46732214e61ec20e3ac0d21b0ab8f9
GabriellyAssuncao <gabriellyassuncaorodrigues@gmail.com>
2022-04-23 11:58:50 -0300

    [FIX] (Issue #274) : correção
    Corrigindo tamanho dos componentes visuais referentes à RF02

--- src/components/telaRF2.vue
+++ src/components/telaRF2.vue
@@ -1,25 +1,15 @@
 <template>
   <body class="div">
     <div class="bloco">
-      <h1 class="titulo">DISCIPLINAS CURSADAS</h1>
-      <h1 class="subtitulo">DISCIPLINAS OBRIGATÓRIAS</h1>
+      <h1 class="titulo">Disciplinas Cursadas</h1>
       <div class="conteudo">
         <!-- local para colocar o resultado das diciplinas obrigatorias cursadas -->
       </div>
-      <h1 class="subtitulo">DISCIPLINAS OPTATIVAS</h1>
-      <div class="conteudo">
-        <!-- local para colocar o resultado das diciplinas optativas cursadas -->
-      </div>
     </div>
     <div class="bloco">
-      <h1 class="titulo">DISPONÍVEIS PARA O PRÓXIMO SEMESTRE</h1>
-      <h1 class="subtitulo">DISCIPLINAS OBRIGATÓRIAS</h1>
+      <h1 class="titulo">Disponíveis Para o Próximo Semestre</h1>
       <div class="conteudo">
         <!-- local para colocar o resultado das diciplinas obrigatorias para o proximo semestre -->
-      </div>
-      <h1 class="subtitulo">DISCIPLINAS OPTATIVAS</h1>
-      <div class="conteudo">
-        <!-- local para colocar o resultado das diciplinas optativas para o proximo semestre -->
       </div>
     </div>
   </body>
@@ -32,8 +22,13 @@ export default {
 </script>
 
 <style>
+.div {
+  margin: auto;
+  width: 90em;
+  height: 27em;
+}
 .bloco {
-  width: 450px;
+  width: 255px;
   float: left;
   background-color: #efefea;
   position: relative;
@@ -41,31 +36,20 @@ export default {
   margin-left: 45px;
 }
 .titulo {
-  font-size: 20px;
+  font-family: Verdana, sans-serif;
+  font-size: 12.32px;
+  font-variant: small-caps;
   font-weight: bold;
+  letter-spacing: 1px;
   color: white;
   background-color: #435382;
   padding-top: 12px;
   padding-bottom: 12px;
   text-align: center;
 }
-.subtitulo {
-  font-size: 18px;
-  font-weight: bold;
-  color: rgb(0, 0, 0);
-  background-color: #c4c4c4;
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: center;
-}
 .conteudo {
-  width: 450px;
-  height: 250px;
+  width: 250px;
+  height: 300px;
   background-color: #efefea;
-}
-.div {
-  margin: auto;
-  width: 90em;
-  height: 27em;
 }
 </style>
